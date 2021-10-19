e6deafd94daf13ecc0aba2c60b918ef1477f6826
Egor <777egorkalinin777@gmail.com>
2021-10-19 21:15:29 +0300

    version 00.02.01

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('hello')
+"""1041"""
+
+max_value = 0
+NoZero = True
+counts = []
+while NoZero:
+    a = int(input("Введите число"))
+
+    if a != 0:
+        counts.append(a)
+    else:
+        NoZero = False
+
+for i in counts:
+    if i % 3 == 0 and i > max_value:
+        max_value = i
+print(max_value)
+
+
+"""1040"""
+
+result = 0
+counts = []
+len_input = int(input("Введите колличество чисел: "))
+
+for i in range(len_input):
+    counts.append(int(input("Введите число: ")))
+
+for count in counts:
+    if count % 6 == 0 and count % 10 == 4:
+        result += 1
+
+print(result)
